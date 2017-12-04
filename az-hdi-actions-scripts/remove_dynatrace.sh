60ac8d86101b0370e5b4312e24c0709d6073620d
Felipe Moz <felipemoz@natura.net>
2017-12-04 16:03:45 -0200

    v1

--- az-hdi-actions-scripts/remove_dynatrace.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-NOW=$(date +"%d/%m/%Y %H:%M")
-echo "----------------------------------------------------" >> sudo /home/sshuser/start-log.out
-echo "REMOVE DYNATRACE SAAS" ${NOW} >> sudo /home/sshuser/start-log.out
-echo "----------------------------------------------------" >> sudo /home/sshuser/start-log.out
-sudo /bin/sh /opt/dynatrace/oneagent/agent/uninstall.sh
-echo "OK" ${NOW} >> sudo /home/sshuser/start-log.out
-echo "----------------------------------------------------" >> sudo /home/sshuser/start-log.out
