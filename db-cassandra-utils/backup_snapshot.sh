a8d2415b975fdaeeb352d93656924ce016ae5666
Felipe Moz <felipemoz@natura.net>
2017-12-07 14:12:22 -0200

    azcopy

--- db-cassandra-utils/backup_snapshot.sh
+++ db-cassandra-utils/backup_snapshot.sh
@@ -94,5 +94,12 @@ cp -prvf "$SNP_VAR" "$_BACKUP_SNAPSHOT_DIR$_SNP_PATH_TRIM";
 done
 
 # Azcopy
+echo $_BACKUP_DIR/$_TODAY_DATE
 
-azcopy --source $_BACKUP_DIR/$_TODAY_DATE --destination "https://$_BLOB.blob.core.windows.net/backup" --dest-key $_BLOB_KEY --recursive
+azcopy \
+    --source $_BACKUP_DIR/$_TODAY_DATE \
+    --destination "https://$_BLOB.blob.core.windows.net/backup" \
+    --dest-key $_BLOB_KEY \
+    --recursive \
+    --parallel-level 2 \
+    --verbose
