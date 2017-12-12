8378d7425598d80aeb76f66e3e6ef9c31d22c4ed
Felipe Moz <felipemoz@natura.net>
2017-12-12 12:36:45 -0200

    kafka

--- az-hdi-actions-scripts/add_modules.sh
+++ az-hdi-actions-scripts/add_modules.sh
@@ -3,11 +3,16 @@ NOW=$(date +"%d/%m/%Y %H:%M")
 echo "----------------------------------------------------" >> /home/sshuser/start-log.out
 echo "PIP / APT-GET " ${NOW} >> /home/sshuser/start-log.out
 echo "----------------------------------------------------" >> /home/sshuser/start-log.out
-sudo apt-get install tree
-sudo apt-get install zip
-sudo apt-get install dos2unix
+sudo apt-get --assume-yes install jq 
+sudo apt-get --assume-yes install sed
+sudo apt-get --assume-yes install tree
+sudo apt-get --assume-yes install zip
+sudo apt-get --assume-yes install dos2unix
 sudo pip install setuptools
 sudo pip install flask
 sudo pip install pystache
+sudo pip install azure-storage==0.20.0
+
+	
 echo "OK " ${NOW} >> /home/sshuser/start-log.out
 echo "----------------------------------------------------" >> /home/sshuser/start-log.out
