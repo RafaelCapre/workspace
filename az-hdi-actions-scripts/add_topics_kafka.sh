9397346b80f12f86e78a5fa773528b0aae903bca
Felipe Moz <felipemoz@natura.net>
2017-12-12 12:43:48 -0200

    topics

--- az-hdi-actions-scripts/add_topics_kafka.sh
+++ az-hdi-actions-scripts/add_topics_kafka.sh
@@ -4,7 +4,9 @@ CLUSTER_PASS=$2
 ZOO=$(curl -u admin:$CLUSTER_PASS -X GET "https://"$CLUSTER_NAME".azurehdinsight.net/api/v1/clusters/"$CLUSTER_NAME"/services/ZOOKEEPER/components/ZOOKEEPER_SERVER" | jq .host_components[].HostRoles.host_name | sed 's/"//g' | sed -z "s/\n/:2181,/g")
 KAFKA_HOME=/usr/hdp/current/kafka-broker/bin/
 
-#Para cria��o de um novo t�pico, basta atualizar o arquivo: "https://hdidevstorage.blob.core.windows.net/startup/topicos.txt" em formato UNX no Azure Explorer.
+#Para cria��o de um novo t�pico, basta atualizar o arquivo: 
+#"https://hdidevstorage.blob.core.windows.net/startup/topicos.txt" em formato UNX no Azure Explorer.
+
 mkdir /home/sshuser/startup/
 cd /home/sshuser/startup/
 sudo rm -rf topicos.txt
@@ -15,7 +17,12 @@ echo $ZOO
 array=( ${array[@]} `cat /home/sshuser/startup/topicos.txt` )
 for i in "${array[@]}"
 	do
-    	$KAFKA_HOME/kafka-topics.sh --zookeeper $ZOO --create --if-not-exists --topic $i  --partitions 2 --replication-factor 2 
+    	$KAFKA_HOME/kafka-topics.sh \
+            --zookeeper $ZOO \
+            --create --if-not-exists \
+            --topic $i \
+            --partitions 2 \
+            --replication-factor 2 
 	done
 
 echo "*** Lista de Tópicos Criados ***"
